81fd2983734e6728f3559b2746791d43b93186aa
younos1986 <47290994+younos1986@users.noreply.github.com>
2020-01-13 10:16:45 +0100

    this line is added by user in github editor

--- EventStoreSample/Program.cs
+++ EventStoreSample/Program.cs
@@ -20,5 +20,7 @@ namespace EventStoreSample
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+        
+        // this line is added by user in github editor
     }
 }
